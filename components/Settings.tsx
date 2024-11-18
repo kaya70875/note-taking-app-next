3a7cd5f98d34ba13ad6db418cbf797e8d3207d9d
kaya70875 <shortsforturkey@gmail.com>
2024-11-18 21:19:18 +0300

    create settings component

---
+++ components/Settings.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import SvgIcon from './reusables/SvgIcon'
+import ThemeSettingsComponent from './ThemeSettingsComponent'
+
+interface SettingsProps {
+    settingType: 'Color Theme' | 'Font Theme' | 'Change Password' | 'Logout'
+}
+
+export default function Settings({ settingType }: SettingsProps) {
+
+    const colorOptions = [
+        {
+            name: 'Light Mode',
+            desc: 'Pick a clean and classic light theme',
+            svg: (<SvgIcon path='sun' />)
+        },
+        {
+            name: 'Dark Mode',
+            desc: 'Pick a dark and classic dark theme',
+            svg: (<SvgIcon path='moon' />)
+        },
+        {
+            name: 'System',
+            desc: 'Adapt the theme based on your system settings',
+            svg: (<SvgIcon path='system-theme' />)
+        }
+    ]
+
+    const themeOptions = [
+        {
+            name: 'Sans-serif',
+            desc: 'Clean and modern, easy to read',
+            svg: (<SvgIcon path='font-sans-serif' />)
+        },
+        {
+            name: 'Serif',
+            desc: 'Classic and elegant, great for reading',
+            svg: (<SvgIcon path='font-serif' />)
+        },
+        {
+            name: 'Monospace',
+            desc: 'Code-like , great for a technical vibe',
+            svg: (<SvgIcon path='font-monospace' />)
+        }
+    ]
+
+    return (
+        <div className='w-full h-full p-10'>
+            {settingType === 'Color Theme' && (
+                <ThemeSettingsComponent themeList={colorOptions} themeType={settingType} />
+            )}
+            {settingType === 'Font Theme' && (
+                <ThemeSettingsComponent themeList={themeOptions} themeType={settingType} />
+            )}
+        </div>
+    )
+}
