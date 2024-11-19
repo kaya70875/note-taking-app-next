978e6c94b0e7d3989553a293a3ccbe8322b24843
kaya70875 <shortsforturkey@gmail.com>
2024-11-19 13:19:55 +0300

    fix typescript error and define a key for mapping

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -5,9 +5,9 @@ import SvgIcon from '@components/reusables/SvgIcon';
 import Settings from '@components/Settings';
 import React, { useState } from 'react'
 
-export default function page() {
+export default function Page() {
     const [searchQuery, setSearchQuery] = useState('');
-    const [activeItem, setActiveItem] = useState('Color Theme');
+    const [activeItem, setActiveItem] = useState<'Color Theme' | 'Font Theme' | 'Change Password' | 'Logout'>('Color Theme');
 
     const settingsItems = [
         {
@@ -26,7 +26,7 @@ export default function page() {
             name: 'Logout',
             svg: (<SvgIcon path='logout' />)
         }
-    ]
+    ] as const;
 
     return (
         <div className='w-full h-full'>
@@ -34,7 +34,7 @@ export default function page() {
             <div className='flex w-full h-full'>
                 <section className="settings-section flex flex-col p-6 gap-8 w-4/12 border-r border-neutral-300 h-full">
                     {settingsItems.map(settings => (
-                        <button className={`flex items-center justify-between p-2 rounded-lg w-3/4 ${activeItem === settings.name ? 'bg-neutral-200' : ''}`} onClick={() => setActiveItem(settings.name)}>
+                        <button key={settings.name} className={`flex items-center justify-between p-2 rounded-lg w-3/4 ${activeItem === settings.name ? 'bg-neutral-200' : ''}`} onClick={() => setActiveItem(settings.name)}>
                             <div className='flex items-center gap-2'>
                                 <div className="icon">{settings.svg}</div>
                                 <p>{settings.name}</p>
@@ -46,8 +46,6 @@ export default function page() {
 
                 <Settings settingType={activeItem} />
             </div>
-
-
         </div>
     )
 }
