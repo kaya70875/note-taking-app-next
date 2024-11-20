9669c977df85368d2c2a22ad62cc24ca5e5078ef
kaya70875 <shortsforturkey@gmail.com>
2024-11-20 15:39:17 +0300

    add SessionProvider and get current session

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,37 +5,37 @@ import Sidebar from "@components/Sidebar";
 import { ArchiveProvider } from "@context/ArchiveContext";
 import { ActiveSidebarTagContextProvider } from "@context/ActiveSidebarTagContext";
 import { ToastProvider } from "@context/ToastContext";
-
-export const metadata: Metadata = {
-  title: "Note Taking App",
-  description: "A simple note taking app",
-};
+import SessionProvider from "../../providers/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@lib/auth";
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <body
         className={inter.className}
       >
-        <ToastProvider>
-          <ActiveSidebarTagContextProvider>
-            <ArchiveProvider>
-              <main className="app">
-                <Sidebar />
-                {children}
-              </main>
-            </ArchiveProvider>
-          </ActiveSidebarTagContextProvider>
-        </ToastProvider>
-
-
-
+        <SessionProvider session={session}>
+          <ToastProvider>
+            <ActiveSidebarTagContextProvider>
+              <ArchiveProvider>
+                <main className="app">
+                  <Sidebar />
+                  {children}
+                </main>
+              </ArchiveProvider>
+            </ActiveSidebarTagContextProvider>
+          </ToastProvider>
+        </SessionProvider>
       </body>
     </html>
   );
