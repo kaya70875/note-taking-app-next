749eeaa4559e073323fd264c6bcfbda9253078d7
kaya70875 <shortsforturkey@gmail.com>
2024-11-21 15:32:44 +0300

    adding dark mode for settings sidebar

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -3,6 +3,11 @@
 import Navbar from '@components/Navbar'
 import SvgIcon from '@components/reusables/SvgIcon';
 import Settings from '@components/Settings';
+import ChevronRight from '@components/svgIcons/ChevronRight';
+import IconFont from '@components/svgIcons/IconFont';
+import IconLock from '@components/svgIcons/IconLock';
+import IconLogout from '@components/svgIcons/IconLogout';
+import IconSun from '@components/svgIcons/IconSun';
 import React, { useState } from 'react'
 
 export default function Page() {
@@ -12,19 +17,19 @@ export default function Page() {
     const settingsItems = [
         {
             name: 'Color Theme',
-            svg: (<SvgIcon path='sun' />)
+            svg: (<IconSun props={{color : 'text-neutral-950 dark:text-neutral-100'}} />)
         },
         {
             name: 'Font Theme',
-            svg: (<SvgIcon path='font' />)
+            svg: (<IconFont props={{color : 'text-neutral-950 dark:text-neutral-100'}} />)
         },
         {
             name: 'Change Password',
-            svg: (<SvgIcon path='lock' />)
+            svg: (<IconLock props={{color : 'text-neutral-950 dark:text-neutral-100'}} />)
         },
         {
             name: 'Logout',
-            svg: (<SvgIcon path='logout' />)
+            svg: (<IconLogout props={{color : 'text-neutral-950 dark:text-neutral-100'}} />)
         }
     ] as const;
 
@@ -32,14 +37,14 @@ export default function Page() {
         <div className='w-full h-full'>
             <Navbar header='Settings' setSearchQuery={setSearchQuery} />
             <div className='flex w-full h-full'>
-                <section className="settings-section flex flex-col p-6 gap-8 w-4/12 border-r border-neutral-300 h-full">
+                <section className="settings-section flex flex-col p-6 gap-8 w-4/12 border-r border-neutral-300 dark:border-neutral-700 h-full">
                     {settingsItems.map(settings => (
-                        <button key={settings.name} className={`flex items-center justify-between p-2 rounded-lg w-3/4 ${activeItem === settings.name ? 'bg-neutral-200' : ''}`} onClick={() => setActiveItem(settings.name)}>
+                        <button key={settings.name} className={`flex items-center justify-between p-2 rounded-lg w-3/4 ${activeItem === settings.name ? 'bg-neutral-200 dark:bg-neutral-700' : ''}`} onClick={() => setActiveItem(settings.name)}>
                             <div className='flex items-center gap-2'>
-                                <div className="icon">{settings.svg}</div>
-                                <p>{settings.name}</p>
+                                <div className={`icon ${activeItem === settings.name ? 'text-blue-500' : 'text-neutral-100'}`}>{settings.svg}</div>
+                                <p className='text-neutral-950 dark:text-neutral-100'>{settings.name}</p>
                             </div>
-                            {settings.name === activeItem && <SvgIcon path='chevron-right' />}
+                            {settings.name === activeItem && <ChevronRight props={{color : 'text-neutral-950 dark:text-neutral-100'}} />}
                         </button>
                     ))}
                 </section>
---
+++ components/svgIcons/IconSun.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+const IconSun = ({props} : {props: React.SVGProps<SVGSVGElement>}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    className={props.className}
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M12.055 3v1.372m0 15.256V21m9-9h-1.372M4.427 12H3.055m15.364-6.364-.97.97M6.66 17.394l-.97.97m12.728 0-.97-.97M6.66 6.606l-.97-.97"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M12.055 7.805a4.195 4.195 0 1 1 0 8.39 4.195 4.195 0 0 1 0-8.39Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default IconSun
