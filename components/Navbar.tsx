d9cbc74cfa2697c385c16a692d9c178968bfe6a2
kaya70875 <shortsforturkey@gmail.com>
2024-11-25 21:05:31 +0300

    add navbar headers for paths

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,47 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SvgIcon from './reusables/SvgIcon'
-import { useRouter } from 'next/navigation';
-import { useActiveSidebarTag } from '@context/ActiveSidebarTagContext';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 interface NavbarProps {
-    header?: 'All Notes' | 'Archived Notes' | 'Notes Tagged:' | 'Settings' | 'Showing Results For:';
     searchQuery?: string;
     setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Navbar({ header = 'All Notes', setSearchQuery, searchQuery}: NavbarProps) {
+export default function Navbar({ setSearchQuery, searchQuery}: NavbarProps) {
     const router = useRouter();
-    const {activeSidebarTag} = useActiveSidebarTag();
+    const pathName = usePathname();
+    const searchParams = useSearchParams();
+
+    const activeSidebarTag = searchParams.get('tag');
+    
+    const [navHeader , setNavHeader] = useState('');
 
     const navigateToSettings = () => {
         router.push('/settings');
     }
 
+    useEffect(() => {
+        if(pathName === '/notes' && !searchParams.has('tag')) {
+            setNavHeader('All Notes');
+        }
+
+        else if(searchParams.has('tag')) {
+            setNavHeader('Notes Tagged:');
+        }
+
+        else if(pathName.includes('archived')) {
+            setNavHeader('Archived Notes');
+        }
+    }, [pathName , searchParams]);
+
     return (
         <div className='w-full flex items-center justify-between p-8 border-b border-neutral-300 dark:border-neutral-700'>
             <header className='font-bold text-2xl flex gap-2'>
-                <h1 className='text-neutral-950 dark:text-neutral-50 font-bold'>{header}</h1>
-                <p className='text-neutral-950 dark:text-neutral-50'>{header === 'Notes Tagged:' && activeSidebarTag && activeSidebarTag}</p>
-                <p>{header === 'Showing Results For:' ? searchQuery : ''}</p>
+                <h1 className='text-neutral-950 dark:text-neutral-50 font-bold'>{navHeader}</h1>
+                <p className='text-neutral-950 dark:text-neutral-50'>{navHeader === 'Notes Tagged:' && activeSidebarTag && activeSidebarTag}</p>
+                <p>{navHeader === 'Showing Results For:' ? searchQuery : ''}</p>
             </header>
             <div className='flex items-center gap-8 w-2/6'>
                 <div className='relative w-full border border-neutral-300 dark:border-neutral-700 rounded-lg overflow-hidden'>
