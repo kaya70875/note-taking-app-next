0d4ab8aaf764270894ab6b8cd00a9071fdec8b04
kaya70875 <shortsforturkey@gmail.com>
2024-11-26 16:03:11 +0300

    render tabletLayout component conditionally

--- app/(main)/(base)/layout.tsx
+++ app/(main)/(base)/layout.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import AllNotes from "@components/AllNotes";
+import BottomNavbar from "@components/layoutShift/BottomNavbar";
+import TabletLayout from "@components/layoutShift/TabletLayout";
 import Navbar from "@components/Navbar";
 import IconArchive from "@components/svgIcons/IconArchive";
 import IconDelete from "@components/svgIcons/IconDelete";
 import IconRestore from "@components/svgIcons/IconRestore";
+import LogoSvg from "@components/svgIcons/LogoSvg";
 import { useToast } from "@context/ToastContext";
 import useNoteActions from "@hooks/useNotActions";
+import useScreenSize from "@hooks/useScreenSize";
 import { useParams, usePathname, useRouter } from "next/navigation";
 import React, { useState } from "react";
 
@@ -18,9 +22,10 @@ export default function Layout({
     children,
 }: LayoutProps) {
 
-    const {showToast} = useToast();
-    const {deleteNote , setArchivedNotes} = useNoteActions();
-    
+    const { showToast } = useToast();
+    const { deleteNote, setArchivedNotes } = useNoteActions();
+    const { isTablet } = useScreenSize();
+
     const pathName = usePathname();
     const router = useRouter();
 
@@ -29,9 +34,9 @@ export default function Layout({
 
     const [searchQuery, setSearchQuery] = useState('');
 
-    const handleDeleteNote = async (noteId: string) => {
+    const handleDeleteNote = async () => {
         if (activeNoteId) {
-            await deleteNote(noteId);
+            await deleteNote(activeNoteId as string);
             // Give a success toast message here.
             showToast('Note deleted successfully!', 'success');
             router.push('/notes');
@@ -43,46 +48,64 @@ export default function Layout({
         }
     };
 
-    const handleArchiveNote = async (noteId: string) => {
+    const handleArchiveNote = async () => {
         if (activeNoteId) {
             // Archive the note here.
-            setArchivedNotes(noteId, !isArchiveOpen());
+            setArchivedNotes(activeNoteId as string, !isArchiveOpen());
             // Give a success toast message here.
             showToast('Note archived successfully!', 'success');
         }
     }
-    
+
     const isArchiveOpen = () => {
         return pathName.includes('/archived');
-    }   
+    }
 
     return (
         <div className="w-full max-h-full">
-            <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-            <div className="flex w-full h-[89.5%] dark:bg-neutral-950">
-                <section className="notes-section flex flex-col items-center p-6 xl:p-4 lg:p-2 gap-4 w-3/12 border-r border-neutral-300 dark:border-neutral-700 h-full">
-                    <button className="primary-btn w-full lg:text-sm" onClick={() => router.push('/create')}>
-                        + Create New Note
-                    </button>
-
-                <AllNotes searchQuery={searchQuery} />
-                </section>
-
-                {children}
-                <section className="archive-section w-1/5 p-4 lg:p-2 flex justify-center">
-                    <div className="buttons flex flex-col gap-4 w-full">
-                        <button onClick={() => handleArchiveNote(activeNoteId as string)} className="flex lg:flex-col items-center gap-2 border border-neutral-300 dark:border-neutral-700 p-2 rounded-lg">
-                            {isArchiveOpen() ? <IconRestore props={{ color: 'text-neutral-950 dark:text-neutral-100' }} /> : <IconArchive props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />}
-                            <p className="text-neutral-950 dark:text-neutral-100">{isArchiveOpen() ? 'Restore Note' : 'Archive Note'}</p>
-                        </button>
-
-                        <button onClick={() => handleDeleteNote(activeNoteId as string)} className="flex lg:flex-col items-center gap-2 border border-neutral-300 dark:border-neutral-700 p-2 rounded-lg">
-                            <IconDelete props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />
-                            <p className="text-neutral-950 dark:text-neutral-100">Delete Note</p>
-                        </button>
+            {isTablet ? (
+                <div className="flex flex-col justify-center h-full bg-neutral-200">
+                    <div className="logo p-6">
+                        <LogoSvg props={{ color: 'text-neutral-950 dark:text-neutral-50' }} />
                     </div>
-                </section>
-            </div>
+                    <TabletLayout>
+                        {children}
+                    </TabletLayout>
+
+                    <div className="bottom-navbar bg-neutral-100">
+                        <BottomNavbar />
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+                    <div className="flex w-full h-[89.5%] dark:bg-neutral-950">
+                        <section className="notes-section flex flex-col items-center p-6 xl:p-4 lg:p-2 gap-4 w-3/12 border-r border-neutral-300 dark:border-neutral-700 h-full">
+                            <button className="primary-btn w-full lg:text-sm" onClick={() => router.push('/create')}>
+                                + Create New Note
+                            </button>
+
+                            <AllNotes searchQuery={searchQuery} />
+                        </section>
+
+                        {children}
+                        <section className="archive-section w-1/5 p-4 lg:p-2 flex justify-center">
+                            <div className="buttons flex flex-col gap-4 w-full">
+                                <button onClick={() => handleArchiveNote} className="flex lg:flex-col items-center gap-2 border border-neutral-300 dark:border-neutral-700 p-2 rounded-lg">
+                                    {isArchiveOpen() ? <IconRestore props={{ color: 'text-neutral-950 dark:text-neutral-100' }} /> : <IconArchive props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />}
+                                    <p className="text-neutral-950 dark:text-neutral-100">{isArchiveOpen() ? 'Restore Note' : 'Archive Note'}</p>
+                                </button>
+
+                                <button onClick={() => handleDeleteNote} className="flex lg:flex-col items-center gap-2 border border-neutral-300 dark:border-neutral-700 p-2 rounded-lg">
+                                    <IconDelete props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />
+                                    <p className="text-neutral-950 dark:text-neutral-100">Delete Note</p>
+                                </button>
+                            </div>
+                        </section>
+                    </div>
+                </>
+            )}
+
         </div>
 
     )
