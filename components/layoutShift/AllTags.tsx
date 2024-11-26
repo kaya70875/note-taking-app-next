11bd0895b281d1dacc54d679e3cb33810d1cdce4
kaya70875 <shortsforturkey@gmail.com>
2024-11-26 16:01:52 +0300

    create allTags component for showing all tags for non-desktop sizes

---
+++ components/layoutShift/AllTags.tsx
@@ -0,0 +1,22 @@
+import useFetch from '@hooks/useFetch';
+import { TagsResponse } from '../../types/notes';
+import React from 'react'
+import TagSvg from '@components/svgIcons/TagSvg';
+import Link from 'next/link';
+
+export default function AllTags() {
+
+    const { data, loading, error } = useFetch<TagsResponse>('/api/getAllTags');
+    const tags = data?.data;
+
+    return (
+        <div className='flex flex-col gap-4'>
+            {tags?.map(tag => (
+                <Link href={`/notes?tag=${tag}`} key={tag} className='flex items-center gap-2'>
+                    <TagSvg props={{color : 'text-neutral-950 dark:text-neutral-100'}} />
+                    <p className='text-sm'>{tag}</p>
+                </Link>
+            ))}
+        </div>
+    )
+}
