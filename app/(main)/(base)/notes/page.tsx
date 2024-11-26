7ee4cf21e7488de234c5dad7bb7094146fbd4fb4
kaya70875 <shortsforturkey@gmail.com>
2024-11-26 20:49:58 +0300

    update style and fix some bugs

--- app/(main)/(base)/notes/page.tsx
+++ app/(main)/(base)/notes/page.tsx
@@ -1,20 +1,8 @@
 'use client';
 
-import useScreenSize from '@hooks/useScreenSize'
+import NotChoosed from '@components/reusables/NotChoosed';
 import React from 'react'
 
 export default function page() {
-
-    const { isTablet } = useScreenSize();
-
-    return (
-        <div className='content-section w-full flex flex-1 flex-col gap-4 p-4 lg:p-0 border-r border-neutral-300 dark:border-neutral-700'>
-            {!isTablet && (
-                <div className="flex flex-col items-center justify-center gap-4 pt-12">
-                    <h2 className="text-2xl text-neutral-950 dark:text-neutral-300">Notes are shown here.</h2>
-                    <p className='dark:text-neutral-300'>Choose a note to get started!</p>
-                </div>
-            )}
-        </div>
-    )
+    return <NotChoosed />
 }
