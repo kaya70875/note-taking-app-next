d882090204bcf9adcfcd066ab1038e5ff2491a49
kaya70875 <shortsforturkey@gmail.com>
2024-11-27 23:19:06 +0300

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,17 +6,12 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import useNavHeader from '@hooks/useNavHeader';
 
 interface NavbarProps {
-    searchQuery?: string;
-    setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+    setSearchQuery?: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Navbar({ setSearchQuery, searchQuery }: NavbarProps) {
+export default function Navbar({ setSearchQuery }: NavbarProps) {
     const router = useRouter();
-    const searchParams = useSearchParams();
-
     const { navHeader } = useNavHeader();
-
-    const activeSidebarTag = searchParams.get('tag');
 
     const navigateToSettings = () => {
         router.push('/settings');
@@ -26,15 +21,13 @@ export default function Navbar({ setSearchQuery, searchQuery }: NavbarProps) {
         <div className='w-full flex items-center justify-between p-8 border-b border-neutral-300 dark:border-neutral-700'>
             <header className='font-bold text-2xl flex gap-2'>
                 <h1 className='text-neutral-950 dark:text-neutral-50 font-bold'>{navHeader}</h1>
-                <p className='text-neutral-950 dark:text-neutral-50'>{navHeader === 'Notes Tagged:' && activeSidebarTag && activeSidebarTag}</p>
-                <p>{navHeader === 'Showing Results For:' ? searchQuery : ''}</p>
             </header>
             <div className='flex items-center gap-8 w-2/6'>
                 <div className='relative w-full border border-neutral-300 dark:border-neutral-700 rounded-lg overflow-hidden'>
                     <div className='absolute top-1/2 -translate-y-1/2 left-2 cursor-pointer'>
                         <SvgIcon path='search' />
                     </div>
-                    <input type="text" placeholder='Search by title, content, or tags...' className='w-full p-3 pl-12' onChange={(e) => setSearchQuery(e.target.value)} />
+                    <input type="text" placeholder='Search by title, content, or tags...' className='w-full p-3 pl-12' onChange={(e) => setSearchQuery && setSearchQuery(e.target.value)} />
                 </div>
                 <div className='cursor-pointer' onClick={() => navigateToSettings()}>
                     <SvgIcon path='settings' />
