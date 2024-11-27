f97d416e7d2f3adad1e69355a6fcb3331f9ae1c4
kaya70875 <shortsforturkey@gmail.com>
2024-11-27 14:40:47 +0300

    create arrowLeft svg

---
+++ components/svgIcons/ArrowLeft.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+const ArrowLeft = ({props} : {props: React.SVGProps<SVGSVGElement>}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    className={props.className}
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M15.75 20.414 7.336 12l8.414-8.414L17.164 5l-7 7 7 7-1.414 1.414Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default ArrowLeft
