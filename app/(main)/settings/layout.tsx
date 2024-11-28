65b67520173d54a945e4f68fe173fd200250460a
kaya70875 <shortsforturkey@gmail.com>
2024-11-28 15:23:16 +0300

    apply conditional rendering for layout page

--- app/(main)/settings/layout.tsx
+++ app/(main)/settings/layout.tsx
@@ -2,14 +2,11 @@
 
 import BottomNavbar from "@components/layoutShift/BottomNavbar";
 import Navbar from "@components/Navbar";
-import ChevronRight from "@components/svgIcons/ChevronRight";
 import useScreenSize from "@hooks/useScreenSize";
-import IconSun from "@components/svgIcons/IconSun";
-import IconFont from "@components/svgIcons/IconFont";
-import IconLock from "@components/svgIcons/IconLock";
-import IconLogout from "@components/svgIcons/IconLogout";
-import { usePathname, useRouter } from "next/navigation";
 import LogoSvg from "@components/svgIcons/LogoSvg";
+import SettingsSidebar from "@components/layoutShift/SettingsSidebar";
+import { usePathname } from "next/navigation";
+import Back from "@components/layoutShift/Back";
 
 export default function RootLayout({
     children,
@@ -17,34 +14,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
 
-    const settingsItems = [
-        {
-            name: 'Color Theme',
-            route: '/theme',
-            svg: (<IconSun props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />)
-        },
-        {
-            name: 'Font Theme',
-            route: '/font',
-            svg: (<IconFont props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />)
-        },
-        {
-            name: 'Change Password',
-            route: '/reset',
-            svg: (<IconLock props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />)
-        },
-        {
-            name: 'Logout',
-            route: '/login',
-            svg: (<IconLogout props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />)
-        }
-    ] as const;
-
     const { isTablet } = useScreenSize();
-    const router = useRouter();
     const pathName = usePathname();
-
-    const activeItem = settingsItems.find(item => pathName.includes(`settings${item.route}`))?.name; // Find the active item based on the pathname
 
     return (
         <div className="w-full h-full">
@@ -56,22 +27,12 @@ export default function RootLayout({
             <div className='w-full h-full lg:flex flex-col-reverse rounded-lg'>
                 {isTablet ? (<BottomNavbar />) : (<Navbar />)}
                 <div className='flex w-full h-full'>
-                    <section className="settings-section flex flex-col p-6 gap-8 w-4/12 xxl:w-5/12 xl:w-7/12 border-r border-neutral-300 dark:border-neutral-700 h-full">
-                        {settingsItems.map(settings => (
-                            <button key={settings.name} className={`flex items-center justify-between p-2 rounded-lg w-3/4 ${activeItem === settings.name ? 'bg-neutral-200 dark:bg-neutral-700' : ''}`} onClick={() => {
-                                router.push(`/settings${settings.route}`);
-                            }}>
-                                <div className='flex items-center gap-2'>
-                                    <div className={`icon ${activeItem === settings.name ? 'text-blue-500' : ''}`}>{settings.svg}</div>
-                                    <p className='text-neutral-950 dark:text-neutral-100'>{settings.name}</p>
-                                </div>
-                                {settings.name === activeItem && <ChevronRight props={{ color: 'text-neutral-950 dark:text-neutral-100' }} />}
-                            </button>
-                        ))}
-                    </section>
-                    <div className="w-full h-full p-10">
-                        {children}
-                    </div>
+                    {pathName === '/settings' ? <SettingsSidebar /> : ( // İf route is settings render sidebar only if not (settings/theme for ex.) render children content only.
+                        <div className="w-full h-full p-10 lg:p-6 lg:flex flex-col gap-2">
+                            <Back />
+                            {children}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,10 +1,19 @@
+'use client';
+
+import useScreenSize from '@hooks/useScreenSize'
 import React from 'react'
 
-export default function page() {
+export default function Page() {
+    const { isTablet } = useScreenSize();
+
     return (
-        <div className='flex flex-col gap-2 items-center justify-center'>
-            <h2>Settings</h2>
-            <p>Set your preferences to customize your experience.</p>
-        </div>
-    )
+        <>
+            {!isTablet && (
+                <div className='flex flex-col gap-2 items-center justify-center'>
+                    <h2>Settings</h2>
+                    <p>Set your preferences to customize your experience.</p>
+                </div>
+            )}
+        </>
+    );
 }
