49591cc38fa72b04021671fe5145f88f172f920a
kaya70875 <shortsforturkey@gmail.com>
2024-11-28 18:52:53 +0300

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-    const protectedRoutes = ['/settings', '/notes' , '/archived' , '/'];
+    const protectedRoutes = ['/settings', '/notes' , '/archived'];
+    const nowAllowed = ['/'];
     const currentPath = req.nextUrl.pathname;
+
     const isProtectedRoute = protectedRoutes.includes(currentPath);
+    const notAllowed = nowAllowed.includes(currentPath);
 
     if(isProtectedRoute) {
         const sessionToken = req.cookies.get('next-auth.session-token') || req.cookies.get('__Secure-next-auth.session-token');
@@ -11,6 +14,10 @@ export default async function middleware(req: NextRequest) {
         if(!sessionToken) {
             return NextResponse.redirect(new URL('/login', req.url));
         }
+    }
+
+    else if(notAllowed) {
+        return NextResponse.redirect(new URL('/notes', req.url));
     }
 
     return NextResponse.next();
