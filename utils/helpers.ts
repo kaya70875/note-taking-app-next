5e5acd8e5326e3e95453362c07f733868e4edee1
kaya70875 <shortsforturkey@gmail.com>
2024-11-28 18:14:01 +0300

    create a helper function for getting full url

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,16 @@
+import { usePathname, useSearchParams } from "next/navigation";
+
 export default function convertDate(dateString : Date) {
     const date = new Date(dateString);
 
     const fullDate = date.toLocaleDateString('en-US' , {year: "numeric", month: "long", day: "numeric" });
 
     return fullDate;
+}
+
+export function getFullUrl() { // Gets full url from url section.
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    return `${pathname}${searchParams.toString() ? `?${searchParams.toString()}` : ''}`;
 }
