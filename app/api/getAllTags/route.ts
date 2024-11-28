abc01bf9eea2397d39cccb033676a975c168c99e
kaya70875 <shortsforturkey@gmail.com>
2024-11-28 18:53:22 +0300

    get tags from database based on userId

--- app/api/getAllTags/route.ts
+++ app/api/getAllTags/route.ts
@@ -1,12 +1,17 @@
+import { authOptions } from "@lib/auth";
 import Note from "@models/note";
 import { connectToDB } from "@utils/database";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function GET(request : Request) {
     try {
+        const session = await getServerSession(authOptions);
         await connectToDB();
 
-        const tagsArray = await Note.find({} , {tags : 1 , _id : 0}).lean();
+        const userId = session?.user.id;
+
+        const tagsArray = await Note.find({ userId } , {tags : 1 , _id : 0}).lean();
         
         const tags = [...new Set(tagsArray.flatMap((note) => note.tags))];
 
