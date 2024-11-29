e3bf9e8000bf67afe82138a30bac09c5cb4fce95
kaya70875 <shortsforturkey@gmail.com>
2024-11-29 19:59:44 +0300

    update tags section style

--- components/layoutShift/AllTags.tsx
+++ components/layoutShift/AllTags.tsx
@@ -3,19 +3,28 @@ import { TagsResponse } from '../../types/notes';
 import React from 'react'
 import TagSvg from '@components/svgIcons/TagSvg';
 import Link from 'next/link';
+import { CircularProgress } from '@mui/material';
 
 export default function AllTags() {
 
     const { data, loading, error } = useFetch<TagsResponse>('/api/getAllTags');
     const tags = data?.data;
 
+    if(error) return <p>Error fetching tags</p>;
+
     return (
-        <div className='flex flex-col gap-4 mt-4 overflow-auto'>
+        <div className='flex flex-col gap-4 mt-4 xs:mt-0 overflow-auto'>
+            {loading && <CircularProgress />}
             {tags?.map(tag => (
-                <Link href={`/notes?tag=${tag}`} key={tag} className='flex items-center gap-2'>
-                    <TagSvg props={{color : 'text-neutral-950 dark:text-neutral-100'}} />
-                    <p className='text-sm'>{tag}</p>
-                </Link>
+                <div className='flex flex-col gap-3' key={tag}>
+                    <Link href={`/notes?tag=${tag}`} className='flex items-center gap-2'>
+                        <TagSvg props={{ color: 'text-neutral-950 dark:text-neutral-100'}} strokeWidth={1.4} />
+                        <p>{tag}</p>
+                    </Link>
+
+                    <div className="line hidden lg:block"></div>
+                </div>
+
             ))}
         </div>
     )
