7b858f1d1731d9f2c1651f48cc0bac7a10d8d53e
kaya70875 <shortsforturkey@gmail.com>
2024-11-30 23:03:44 +0300

    update authActions hook

--- hooks/useAuthActions.tsx
+++ hooks/useAuthActions.tsx
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken'
+
 interface Form {
     email: string,
     password: string,
@@ -21,5 +23,48 @@ export const useAuthActions = () => {
         }
     }
 
-    return {handleSignUp}
+    const handleSendResetPassword = async(email : string) => {
+        try {
+            const response = await fetch('/api/sendResetEmail' , {
+                method : 'POST',
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify({email})
+            });
+
+            if(response.ok) {
+               console.log('Reset link sent successfully!'); 
+            } else {
+                console.log('There was an error while sending reset link.');
+            }
+        } catch(e){
+            console.error(e);
+        }
+    }
+
+    const handleResetPassword = async(token : string , newPassword : string) => {
+        try {
+            // verify the token
+
+            const decoded : any = jwt.verify(token as string , process.env.NEXT_PUBLIC_JWT_SECRET as string);
+            const response = await fetch('/api/resetPassword', {
+                method : 'POST',
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify({email : decoded.email , newPassword})
+            });
+
+            if(response.ok) {
+                console.log('Password reset successfully.'); 
+             } else {
+                 console.log('There was an error while resetting password.');
+             }
+    } catch(e : any) {
+        console.error(e);
+    }
+
+    }
+    return {handleSignUp , handleSendResetPassword , handleResetPassword}
 }
