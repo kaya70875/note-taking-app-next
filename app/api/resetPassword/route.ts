b48e203765ada956c357046a501dc9509a56a898
kaya70875 <shortsforturkey@gmail.com>
2024-12-01 15:29:49 +0300

    update resetPassword route

--- app/api/resetPassword/route.ts
+++ app/api/resetPassword/route.ts
@@ -1,35 +1,48 @@
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
-import { NextApiResponse } from "next";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import verifyToken from "@utils/verifyToken";
+import { NextResponse } from "next/server";
 
-export async function POST(req: Request , res : NextApiResponse) {
-    try {
-          await connectToDB();
+export async function POST(req: Request) {
+  try {
+    await connectToDB();
 
-          const body = await req.json();
-          const {email , newPassword} = body;
+    const body = await req.json();
+    const { token, newPassword } = body;
 
-          console.log(email , newPassword);
+    if (!token || !newPassword) {
+      return NextResponse.json(
+        { error: "Missing token or new password" },
+        { status: 400 }
+      );
+    }
 
-          if (!email || !newPassword) {
-            return res.status(400).json({ error: "Email and new password are required" });
-            }
-        
-        const user = await User.findOne({ email });
+    const payload = await verifyToken(token); // Decode and verify the token
+    const email = payload.email; // Extract the email from the token payload
 
-        if(!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
+    const user = await User.findOne({ email });
 
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
 
-        const updatedPassword = await user.updateOne({ password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
 
-        return res.status(200).json({ message: "Password updated successfully" , data : updatedPassword });
+    const updatedPassword = await user.updateOne({
+      password: hashedPassword,
+      new : true,
+    });
 
-    } catch (error) {
-        console.error("Error updating password:", error);
-        return res.status(500).json({ error: "Internal Server Error" });
-    }   
+    return NextResponse.json(
+      { message: "Password updated successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error updating password:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
