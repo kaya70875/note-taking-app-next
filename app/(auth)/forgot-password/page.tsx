8a7dbfb626cc46225dcec7e01f11612730ca6dae
kaya70875 <shortsforturkey@gmail.com>
2024-12-01 20:07:00 +0300

    implement success messages for forgot password and reset password sections

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -4,43 +4,57 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import logo from '@public/images/logo.svg'
 import { useAuthActions } from '@hooks/useAuthActions';
+import SuccessComponent from '@components/SuccessComponent';
 
-export default function page() {
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+export default function Page() {
     const { handleSendResetPassword } = useAuthActions();
 
     const [email, setEmail] = useState('');
     const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
 
     const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
 
-        if (email) {
+        if (regexEmail.test(email)) {
             setLoading(true);
             await handleSendResetPassword(email);
+            setSuccess(true);
             setLoading(false);
+        }
+        else {
+            setSuccess(false);
         }
     }
 
     return (
-        <div className='flex flex-col gap-8 p-12 xs:p-4'>
-            <header className='flex flex-col gap-4 items-center justify-center text-center'>
-                <Image src={logo} alt='logo' className='w-32 h-18' />
-                <div className='flex flex-col gap-1'>
-                    <h1>Forgotten Your Password ?</h1>
-                    <p>Enter your email below, and we'll send you a link to reset it.</p>
+        <>
+            {success ? (
+                <SuccessComponent title='Email sended to your mail!' message='Check your mail for reset link. We"ll send you an mail with a link to reset your password.' />
+            ) : (
+                <div className='flex flex-col gap-8 p-12 xs:p-4'>
+                    <header className='flex flex-col gap-4 items-center justify-center text-center'>
+                        <Image src={logo} alt='logo' className='w-32 h-18' />
+                        <div className='flex flex-col gap-1'>
+                            <h1>Forgotten Your Password ?</h1>
+                            <p>Enter your email below, and we'll send you a link to reset it.</p>
+                        </div>
+                    </header>
+
+                    <div className="input-wrapper">
+                        <label htmlFor="email">Email Address</label>
+                        <input type="text" className='input !w-full' id='email'
+                            placeholder='email@example.com'
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <p className='text-red-500 text-sm'>{regexEmail.test(email) ? '' : 'Please enter a valid email'}</p>
+                    </div>
+
+                    <button onClick={handleClick} disabled={loading} className="primary-btn disabled:opacity-50 disabled:hover:bg-blue-600">Send Reset Link</button>
                 </div>
-            </header>
-
-            <div className="input-wrapper">
-                <label htmlFor="email">Email Address</label>
-                <input type="text" className='input !w-full' id='email'
-                    placeholder='email@example.com'
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-
-            <button onClick={handleClick} disabled={loading} className="primary-btn disabled:opacity-50 disabled:hover:bg-blue-600">Send Reset Link</button>
-        </div>
-    )
-}
+            )}
+        </>
+    );
+}        
--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -5,10 +5,12 @@ import logo from '@public/images/logo.svg'
 import Image from 'next/image'
 import { useSearchParams } from 'next/navigation'
 import { useAuthActions } from '@hooks/useAuthActions'
+import { set } from 'mongoose';
+import SuccessComponent from '@components/SuccessComponent';
 
 export default function page() {
 
-    const {handleResetPassword} = useAuthActions();
+    const { handleResetPassword } = useAuthActions();
 
     const searchParams = useSearchParams();
     const token = searchParams.get('token');
@@ -17,44 +19,54 @@ export default function page() {
     const [confirmPassword, setConfirmPassword] = React.useState('');
     const [loading, setLoading] = React.useState(false);
 
+    const [success, setSuccess] = React.useState(false);
+
     const handleClick = async () => {
 
-        if(newPassword === confirmPassword) {
+        if (newPassword === confirmPassword) {
             setLoading(true);
-            await handleResetPassword(token as string , newPassword);
+            await handleResetPassword(token as string, newPassword);
+            setSuccess(true);
             setLoading(false);
         }
 
         else {
             alert('Passwords do not match');
+            setSuccess(false);
         }
     }
 
     return (
-        <div className='flex flex-col gap-8 p-12 xs:p-4'>
-            <header className='flex flex-col gap-4 items-center justify-center text-center'>
-                <Image src={logo} alt='logo' className='w-32 h-18' />
-                <div className='flex flex-col gap-1'>
-                    <h1>Reset Your Password</h1>
-                    <p>Choose a new password to secure your account.</p>
+        <>
+            {!success ? (
+                <div className='flex flex-col gap-8 p-12 xs:p-4'>
+                    <header className='flex flex-col gap-4 items-center justify-center text-center'>
+                        <Image src={logo} alt='logo' className='w-32 h-18' />
+                        <div className='flex flex-col gap-1'>
+                            <h1>Reset Your Password</h1>
+                            <p>Choose a new password to secure your account.</p>
+                        </div>
+                    </header>
+
+                    <div className="input-wrapper">
+                        <label htmlFor="new-pass">New Password</label>
+                        <input type="password" className='input !w-full' id='new-pass'
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="input-wrapper">
+                        <label htmlFor="new-pass-again">Confirm New Password</label>
+                        <input type="password" className='input !w-full' id='new-pass-again'
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </div>
+                    <button onClick={handleClick} disabled={loading} className="primary-btn disabled:opacity-50 disabled:hover:bg-blue-600">Reset Password</button>
                 </div>
-            </header>
+            ) : (
+                <SuccessComponent title='Password Reset Successfully!' message='Your password has been successfully reset. You can now log in with your new password.' />
+            )}
+        </>
 
-            <div className="input-wrapper">
-                <label htmlFor="new-pass">New Password</label>
-                <input type="password" className='input !w-full' id='new-pass'
-                onChange={(e) => setNewPassword(e.target.value)}
-                />
-            </div>
-
-            <div className="input-wrapper">
-                <label htmlFor="new-pass-again">Confirm New Password</label>
-                <input type="password" className='input !w-full' id='new-pass-again'
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-            </div>
-
-            <button onClick={handleClick} disabled={loading} className="primary-btn disabled:opacity-50 disabled:hover:bg-blue-600">Reset Password</button>
-        </div>
     )
 }
