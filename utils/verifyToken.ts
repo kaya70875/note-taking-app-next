7a4043bd6e84abe23b0f4c5c35f114bb3cc7536c
kaya70875 <shortsforturkey@gmail.com>
2024-12-01 15:29:15 +0300

    create verifyToken utils function

---
+++ utils/verifyToken.ts
@@ -0,0 +1,16 @@
+import { jwtVerify } from "jose";
+
+const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
+
+async function verifyToken(token : string) {
+    try {
+        const {payload} = await jwtVerify(token , secret);
+        console.log('Decoded Payload:', payload);
+        return payload;// This contains the decoded information. ex. email
+    } catch(error : any){
+        console.error(error.message);
+        throw new Error('Invalid or expired token');
+    }
+}
+
+export default verifyToken;
