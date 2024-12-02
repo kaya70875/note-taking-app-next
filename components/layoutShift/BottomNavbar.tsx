adf146889d4b16ca14240c60e12a113577ad2985
kaya70875 <shortsforturkey@gmail.com>
2024-12-02 15:05:32 +0300

    add search icon for search button

--- components/layoutShift/BottomNavbar.tsx
+++ components/layoutShift/BottomNavbar.tsx
@@ -1,4 +1,5 @@
 import IconArchive from '@components/svgIcons/IconArchive'
+import IconSearch from '@components/svgIcons/IconSearch'
 import IconSettings from '@components/svgIcons/IconSettings'
 import TagSvg from '@components/svgIcons/TagSvg'
 import { getFullUrl } from '@utils/helpers'
@@ -18,7 +19,7 @@ export default function BottomNavbar() {
         {
             name: 'Search',
             route: '/notes?search=',
-            icon: (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21 7.782v8.435C21 19.165 18.919 21 15.974 21H8.026C5.081 21 3 19.165 3 16.216V7.782C3 4.834 5.081 3 8.026 3h7.948C18.919 3 21 4.843 21 7.782Z" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m15 14-3.002 3L9 14M11.998 17v-7M20.934 7H3.059" /></svg>),
+            icon: (<IconSearch props={{color : 'text-neutral-950 dark:text-neutral-50'}} />),
         },
         {
             name : 'Archive',
