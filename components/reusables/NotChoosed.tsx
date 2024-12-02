d71b90af7f28f75b9e2a38b16c5ff9948fac7b31
kaya70875 <shortsforturkey@gmail.com>
2024-12-02 15:13:54 +0300

    fix a bug that there is a empty content rendering above tags and causes a massive gap

--- components/reusables/NotChoosed.tsx
+++ components/reusables/NotChoosed.tsx
@@ -1,14 +1,15 @@
 import AllNotes from '@components/AllNotes';
 import useScreenSize from '@hooks/useScreenSize';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import React from 'react'
 
 export default function NotChoosed() {
     const { isTablet } = useScreenSize();
     const pathName = usePathname();
+    const searchParams = useSearchParams();
 
     return (
-        <div className={`content-section w-full flex flex-1 flex-col gap-4 p-4 xs:p-1 lg:p-0 border-r lg:border-none border-neutral-300 dark:border-neutral-700 ${pathName !== '/archived' && pathName !== '/notes' ? 'hidden' : 'block'}`}>
+        <div className={`content-section w-full flex flex-1 flex-col gap-4 p-4 xs:p-1 lg:p-0 border-r lg:border-none border-neutral-300 dark:border-neutral-700 ${searchParams.has('tag') ? 'hidden' : 'block'}`}>
             {!isTablet ? (
                 <div className="flex flex-col items-center justify-center gap-4 pt-12">
                     <h2 className="text-2xl text-neutral-950 dark:text-neutral-300">Notes are shown here.</h2>
