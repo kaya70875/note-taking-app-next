6f59cbf7424a0949ca9bce89236aceca1c61b3ad
kaya70875 <shortsforturkey@gmail.com>
2024-12-03 15:15:05 +0300

    update Back component

--- components/layoutShift/Back.tsx
+++ components/layoutShift/Back.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Back() {
     const router = useRouter();
     return (
-        <div className='go-back flex gap-2 items-center' onClick={() => router.back()}>
+        <div className='go-back flex gap-2 items-center cursor-pointer' onClick={() => router.back()}>
             <ArrowLeft props={{ color: 'text-neutral-50 dark:text-neutral-100' }} />
             <p className='text-neutral-600 dark:text-neutral-300'>Go Back</p>
         </div>
