ae3e00f057e1d59d95c07a0625e01bd8e4034649
kaya70875 <shortsforturkey@gmail.com>
2024-12-19 16:29:45 +0300

    create LoadingState component

---
+++ components/reusables/LoadingState.tsx
@@ -0,0 +1,8 @@
+import { CircularProgress } from "@node_modules/@mui/material";
+
+export const LoadingState = () => (
+    <div className='w-full h-full flex items-center justify-center'>
+      <CircularProgress />
+    </div>
+  )
+  
